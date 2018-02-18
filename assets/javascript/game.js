f23c7fcf53ce932b26660291f5fd2790cd6848af
Josh <jhint2424@gmail.com>
2018-02-17 21:50:56 -0700

    final, final, commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,64 +3,74 @@ var wordList = ["homerun", "triple", "double", "single", "walk", "strikeout", "f
 var wins = 0;
 var loss = 0;
 var wrongLetter = [];
-var guessesLeft = 9;
-var underScores = [];
-var userGuesses = [];
+var remainingGuesses = 9;
+var blanks = [];
+var playerGuesses = [];
 var wordRandom;
 //included for guessed letters
 var letterGuessed = [];
 //included after for wins
 var winCounter = 0;
+//end current word??????????????????????????????
+//start new word????????????????????
+//var nextWord;
 //function to choose random word
 function startGame()    {
     wordRandom = wordList[Math.floor(Math.random()*wordList.length)];
 
     for(var i = 0; i < wordRandom.length; i++)  {
-       underScores.push('_');
+       blanks.push('_');
      
     }
     //test
-    console.log(underScores)
+    console.log(blanks)
     //send the blanks to the screen
-    document.getElementById('current-word').innerHTML = underScores.join(" ");
+    document.getElementById('current-word').innerHTML = blanks.join(" ");
 
     //reset
     wrongLetter = [];
-    guessesLeft = 9;
-    //HTML
-    document.getElementById("guesses-remaining").innerHTML = guessesLeft;
+    remainingGuesses = 9;
+    //goes to HTML
+    document.getElementById("guesses-remaining").innerHTML = remainingGuesses;
 }
+//win or lose function written need to end word/start new?
 function winLose()  {
 
     if  (winCounter === wordRandom.length)  {
         alert("That's a W");
+        wins++;
+        document.getElementById("win-total").innerHTML = wins;
+
     }
-    else if (guessesLeft === 0)   {
+    else if (remainingGuesses === 0)   {
         alert("You Must be a Cubs Fan")
     }
 }
 //capture the players input
 document.onkeyup = function (event)  {
-    userGuesses = event.key;
-        console.log(userGuesses);
-    if(wordRandom.indexOf(userGuesses) > -1)   {
+    playerGuesses = event.key;
+    //test
+        console.log(playerGuesses);
+        //win parameters
+    if(wordRandom.indexOf(playerGuesses) > -1)   {
         for (var i = 0; i < wordRandom.length; i++) {
             
-            if (wordRandom[i] === userGuesses)  {
+            if (wordRandom[i] === playerGuesses)  {
             
-                underScores[i] = userGuesses;
-                document.getElementById("current-word").innerHTML = underScores.join(" ");
+                blanks[i] = playerGuesses;
+                document.getElementById("current-word").innerHTML = blanks.join(" ");
                 winCounter++;
                 winLose();
             }
         }
     }
+    //loss paramente
     else    {
-        wrongLetter.push(userGuesses);
-        guessesLeft--;
+        wrongLetter.push(playerGuesses);
+        remainingGuesses--;
         wrongLetter = letterGuessed;
         document.getElementById("letters-guessed").innerHTML = letterGuessed.join(" ");
-        document.getElementById("guesses-remaining").innerHTML = guessesLeft;
+        document.getElementById("guesses-remaining").innerHTML = remainingGuesses;
         console.log(wrongLetter)
         winLose();
     }
@@ -69,3 +79,69 @@ document.onkeyup = function (event)  {
 
 //main game start
 startGame();
+
+////pseudocode
+
+
+//generate a word at random and store it in a variable
+
+//var wordList = ["homerun", "triple", "double", "single", "walk", "strikeout", "foulball", "pitcher", "hitter", "shortstop", "outfielder", "infielder", "bullpen", "dugout", "stadium",];
+
+//var wordRandom = wordList[Math.floor(Math.random()*wordList.length)];
+
+//display the length of the word to the player
+
+//var answerBlank = [];
+
+//for (var i = 0; i < wordRandom.length; i++)   {
+//    answerBlank[i]="_";
+//}
+
+  //  var lettersRemaining = wordRandom.length;
+
+    //does this work? Yes
+//console.log(wordRandom)
+//console.log(answerBlank)
+//console.log(lettersRemaining)
+
+    
+//This function is run when the player presses a key 
+    //determines pressed key
+//document.onkeyup = function(event) {
+   
+  //  playerGuess = event.key;
+
+    //if (playerGuess === null) {
+      //  document.getElementById("alert").innerHTML = "Select a Valid Letter";
+    //}
+
+    //for (var j = 0; j < wordRandom.length; j++) {
+      //  if (wordRandom[j] === playerGuess)  {
+        //    answerBlank[j] = playerGuess;
+          //  lettersRemaining--;
+        //} 
+
+    //}
+
+    //console.log(playerGuess)
+    
+    //}
+
+
+//list of guessed letters, matched letters, display of those ,exists in word... if yes, print yes, if no, print no "push onto new array og guessed letters"
+
+//form, enter single letter, document.ready
+
+
+    //game loop
+
+
+
+//if the guess is correct increment add letter to length of work prompt
+
+//if the guess is incorrect decrease number of guesses remaining by 1
+
+//keep track of guessed letters "letters already guessed"
+//if guesses remaining = 0 then end game as a loss and move on to a new word
+
+//if correct guesses is equal to the length of the word, tell the user they won and start a new game
